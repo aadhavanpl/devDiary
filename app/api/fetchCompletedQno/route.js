0b8acc8c20f1c8c4c8ffef7e5d50471229b2e088
sahelriaz <sahelnriaz@gmail.com>
2023-11-01 16:27:02 +0530

    Add: API to fetch qno's

---
+++ app/api/fetchCompletedQno/route.js
@@ -0,0 +1,41 @@
+import connectMongoDB from '@/lib/mongodb'
+import { users } from '@/models/users'
+import { NextResponse } from 'next/server'
+
+export async function POST(req) {
+	try {
+		const { user_email } = await req.json()
+		await connectMongoDB()
+		const archiveAPI = await users.aggregate([
+			{
+				$match: {
+					user_email: user_email,
+				},
+			},
+			{
+				$unwind: '$problems',
+			},
+			{
+				$match: {
+					'problems.submissions': { $not: { $size: 0 } },
+				},
+			},
+			{
+				$group: {
+					_id: null,
+					qno: { $addToSet: '$problems.qno' },
+				},
+			},
+			{
+				$project: {
+					_id: 0,
+					qno: 1,
+				},
+			},
+		])
+		return NextResponse.json({ qnoAPI }, { message: 'Question numbers fetched' }, { status: 201 })
+	} catch (error) {
+		console.error('Error:', error)
+		return NextResponse.json({ message: 'Error fetching question numbers' }, { status: 500 })
+	}
+}
